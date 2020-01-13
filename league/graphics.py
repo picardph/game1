074d51633519fb80a7202a9ebe2b02e487e050b3
Ira Woodring <irawoodring@gmail.com>
2020-01-13 14:03:45 -0500

    Add basic spritesheet code.

--- league/graphics.py
+++ league/graphics.py
@@ -1,2 +1,41 @@
+
 class SpriteSheet:
-    pass
+    """An object that represents a spritesheet and provides
+    methods to access individual sprites from it.
+
+    There are better ways to create spritesheets.  This code does
+    not allow for packed sprites for instance.  Instead, it forces
+    sprites to be in nice, tiled squares.
+
+    Fields:
+    path = The path to the spritesheet file.
+    tile_size = The number of pixels wide and high the sprites are.  We are forcing square tiles for this engine.
+    per_row = The number of sprites per row on the spritesheet.
+    width = Number of pixels wide of the spritesheet image.
+    height = Number of pixels high of the spritesheet image.
+    """
+    def __init__(self, path, tile_size, per_row):
+        self.path = path
+        self.sheet = pygame.image.load(self.path).convert_alpha()
+        self.tile_size = tile_size
+        self.per_row = per_row
+        self.width, self.height = self.sheet.get_size()
+        self.sprites = self.split_up()
+
+    def split_up(self):
+        sprites = []
+        count = 0
+        for i in range(height // self.tile_size):
+            for j in range(width // self.tile.size):
+                image = self.get_image_num(count)
+                sprites.append(image)
+                count = count + 1
+        return sprites
+
+    def get_image_num(self, num):
+        y = self.tile_size * (num // self.per_row)
+        x = self.tile_size * (num % self.per_row)
+        image = pygame.Surface((self.tile_size, self.tile_size))
+        image.blit(self.sheet, (0, 0), (x, y, x + self.tile_size, y + self.tile_size))
+        return image
+
