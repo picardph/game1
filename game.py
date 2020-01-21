04bacd3e3b0d6822f6c36b5dcdbc60d0df0d70b8
Ira Woodring <irawoodring@gmail.com>
2020-01-20 20:39:32 -0500

    Garbage demo code.

---
+++ game.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import pygame
+import league
+from player import Player
+
+"""This file is garbage. It was a hastily coded mockup
+to demonstrate how to use the engine.  We will be creating
+a Game class that organizes this code better (and is
+reusable).
+"""
+
+# Function to call when colliding with zombie
+def ouch():
+    print("Ouch!  Look out, you #$!@%!@")
+
+def main():
+    e = league.Engine("Sigrid's Quest")
+    e.init_pygame()
+
+    sprites = league.Spritesheet('./assets/base_chip_pipo.png', league.Settings.tile_size, 8)
+    t = league.Tilemap('./assets/world.lvl', sprites, layer = 1)
+    b = league.Tilemap('./assets/background.lvl', sprites, layer = 0)
+    world_size = (t.wide*league.Settings.tile_size, t.high *league.Settings.tile_size)
+    e.drawables.add(t.passable.sprites())
+    e.drawables.add(b.passable.sprites()) 
+    text = "0100     4 lives"
+    font = pygame.font.SysFont("comicsansms", 72)
+    score = font.render(text, True, (0, 0, 0))
+    e.overlay = text
+    p = Player(2, 400, 300)
+    p.blocks.add(t.impassable)
+    p.world_size = world_size
+    p.rect = p.image.get_rect()
+    q = Player(10, 100, 100)
+    q.image = p.image
+    e.objects.append(p)
+    e.objects.append(q)
+    e.drawables.add(p)
+    e.drawables.add(q)
+    c = league.LessDumbCamera(800, 600, p, e.drawables, world_size)
+    #c = league.DumbCamera(800, 600, p, e.drawables, world_size)
+    
+    e.objects.append(c)
+
+    e.collisions[p] = (q, ouch) 
+    pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // league.Settings.gameTimeFactor)
+    e.key_events[pygame.K_a] = p.move_left
+    e.key_events[pygame.K_d] = p.move_right
+    e.key_events[pygame.K_w] = p.move_up
+    e.key_events[pygame.K_s] = p.move_down
+    e.events[pygame.USEREVENT + 1] = q.move_right
+    e.events[pygame.QUIT] = e.stop
+    e.run()
+
+if __name__=='__main__':
+    main()
---
+++ player.py
@@ -0,0 +1,108 @@
+from league import *
+import pygame
+
+class Player(Character):
+    """This is a sample class for a player object.  A player
+    is a character, is a drawable, and an updateable object.
+    This class should handle everything a player does, such as
+    moving, throwing/shooting, collisions, etc.  It was hastily
+    written as a demo but should direction.
+    """
+    def __init__(self, z=0, x=0, y=0):
+        super().__init__(z, x, y)
+        # A unit-less value.  Bigger is faster.
+        self.delta = 512
+        # Where the player is positioned
+        self.x = x
+        self.y = y
+        # The image to use.  This will change frequently
+        # in an animated Player class.
+        self.image = pygame.image.load('zombie.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.rect = self.image.get_rect()
+        # How big the world is, so we can check for boundries
+        self.world_size = (Settings.width, Settings.height)
+        # What sprites am I not allowd to cross?
+        self.blocks = pygame.sprite.Group()
+        # Which collision detection function?
+        self.collide_function = pygame.sprite.collide_circle
+        self.collisions = []
+        # For collision detection, we need to compare our sprite
+        # with collideable sprites.  However, we have to remap
+        # the collideable sprites coordinates since they change.
+        # For performance reasons I created this sprite so we
+        # don't have to create more memory each iteration of
+        # collision detection.
+        self.collider = Drawable()
+        self.collider.image = pygame.Surface([Settings.tile_size, Settings.tile_size])
+        self.collider.rect = self.collider.image.get_rect()
+
+    def move_left(self, time):
+        amount = self.delta * time
+        try:
+            if self.x - amount < 0:
+                raise OffScreenLeftException
+            else:
+                self.x = self.x - amount
+                self.update(0)
+                while(len(self.collisions) != 0):
+                    self.x = self.x + amount
+                    self.update(0)
+        except:
+            pass
+
+    def move_right(self, time):
+        self.collisions = []
+        amount = self.delta * time
+        try:
+            if self.x + amount > self.world_size[0] - Settings.tile_size:
+                raise OffScreenRightException
+            else:
+                self.x = self.x + amount
+                self.update(0)
+                while(len(self.collisions) != 0):
+                    self.x = self.x - amount
+                    self.update(0)
+        except:
+            pass
+
+    def move_up(self, time):
+        self.collisions = []
+        amount = self.delta * time
+        try:
+            if self.y - amount < 0:
+                raise OffScreenTopException
+            else:
+                self.y = self.y - amount
+                self.update(0)
+                if len(self.collisions) != 0:
+                    self.y = self.y + amount
+                    self.update(0)
+                    self.collisions = []
+        except:
+            pass
+
+    def move_down(self, time):
+        amount = self.delta * time
+        try:
+            if self.y + amount > self.world_size[1] - Settings.tile_size:
+                raise OffScreenBottomException
+            else:
+                self.y = self.y + amount
+                self.update(0)
+                if len(self.collisions) != 0:
+                    self.y = self.y - amount
+                    self.update(0)
+                    self.collisions = []
+        except:
+            pass
+
+    def update(self, time):
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.collisions = []
+        for sprite in self.blocks:
+            self.collider.rect.x= sprite.x
+            self.collider.rect.y = sprite.y
+            if pygame.sprite.collide_rect(self, self.collider):
+                self.collisions.append(sprite)
