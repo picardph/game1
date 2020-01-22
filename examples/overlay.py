b1bf738290d665540839516253c723f53ccac7c0
Ira Woodring <irawoodring@gmail.com>
2020-01-22 10:46:10 -0500

    Color background.

--- examples/overlay.py
+++ examples/overlay.py
@@ -7,13 +7,18 @@ class Overlay(league.DUGameObject):
         self._layer = 1000
         self.player = player
         self.font = pygame.font.Font('freesansbold.ttf',32)
-        self.image = self.font.render(str(self.player.health) + "        4 lives", True, (0,0,0))
+        self.image = pygame.Surface([800, 40])
+        self.image.fill((127, 127, 127))
+        self.text = self.font.render(str(self.player.health) + "        4 lives", True, (0,0,0))
+        self.image.blit(self.text, (0, 0))
         self.rect = self.image.get_rect()
-        self.x = 100
-        self.y = 10
-        self.rect.x = 100
-        self.rect.y = 10
+        self.x = 0
+        self.y = 0
+        self.rect.x = 000
+        self.rect.y = 0
         self.static = True
 
     def update(self, deltaTime):
-        self.image = self.font.render(str(self.player.health) + "        4 lives", True, (0,0,0))
+        self.image.fill((127, 127, 127))
+        self.text = self.font.render(str(self.player.health) + "        4 lives", True, (0,0,0))
+        self.image.blit(self.text, (0, 0))
