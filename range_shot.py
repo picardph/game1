c44fb5fdfb6412d908dd066145efec0d0c5a945b
Zac Laymon <laymonz@mail.gvsu.edu>
2020-02-22 13:57:34 -0500

    Overhauled projectile movement. Introduced 'Feature' of controllable non moving projectiles.

--- range_shot.py
+++ range_shot.py
@@ -2,10 +2,11 @@ from league import *
 from league.game_objects import Updateable
 from collision import Collision, Collidable
 import pygame
+from pygame import Vector3
 from NPC_crate import Crate
 
 class Ranged_Shot(Collidable):
-    def __init__(self, z, x, y, scene, direction = "right", melee = False):
+    def __init__(self, z, x, y, scene, direction:Vector3, melee = False):
 
         super().__init__(z, x, y)
 
@@ -16,13 +17,20 @@ class Ranged_Shot(Collidable):
         self.z = 0
         self.x = x
         self.y = y
-        self.image = None
+        self.image = pygame.image.load('./assets/right_shot.png').convert_alpha()
         self.melee = melee
         self.maxRange = 0
         self.moved = 0
+        self.direction = direction
+
+        # If a zero vector is given, delete the projectile.
+        if direction.length == 0:
+            self.scene.despawnObject(self)
+            self.beenDeleted = True
+            return
 
         if melee:
-            self.maxRange = 200
+            self.maxRange = 50
             self.delta = 500
             if direction == "right":
                 self.image = pygame.image.load('./assets/right_slash.png').convert_alpha()
@@ -33,21 +41,14 @@ class Ranged_Shot(Collidable):
             else:
                 self.image = pygame.image.load('./assets/left_slash.png').convert_alpha()
         else:
-            if direction == "right":
-                self.image = pygame.image.load('./assets/right_shot.png').convert_alpha()
-            elif direction == "down":
-                self.image = pygame.image.load('./assets/down_shot.png').convert_alpha()
-            elif direction == "up":
-                self.image = pygame.image.load('./assets/up_shot.png').convert_alpha()
-            else:
-                self.image = pygame.image.load('./assets/left_shot.png').convert_alpha()
+            self.imageSelect()
 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
         # Correct for the size of the projectile to make the bullet more centered from the player's position
-        if direction == "right" or direction == "left":
+        if direction == Vector3(1, 0, 0) or direction == Vector3(-1, 0, 0):
             self.rect.y = self.rect.y - (self.rect.height / 2)
             self.y = self.rect.y
         else:
@@ -59,50 +60,38 @@ class Ranged_Shot(Collidable):
         self.blocks = pygame.sprite.Group()
         # Which collision detection function?
         self.collide_function = pygame.sprite.collide_circle
-        self.collisions = []
 
         self.collider = Drawable()
         self.collider.image = pygame.Surface([Settings.tile_size, Settings.tile_size])
         self.collider.rect = self.collider.image.get_rect()
 
-        self.direction = direction
         self.scene = scene
         self.beenDeleted = False
 
     def update(self):
-        amount = self.delta * Updateable.gameDeltaTime
+        amount = self.delta * Updateable.gameDeltaTime * self.direction
         if self.melee:
-            self.moved += amount
+            self.moved += amount.length()
             if self.moved >= self.maxRange:
                 self.scene.despawnObject(self)
                 return
         try:
-            if self.direction == "right":
-                self.x = self.x + amount
-                self.rect.x = self.x
-                if self.x > self.world_size[0] - Settings.tile_size:
-                   raise OffScreenException
-            elif self.direction == "left":
-                self.x = self.x - amount
-                self.rect.x = self.x
-                if self.x < 0:
-                    raise OffScreenException
-            elif self.direction == "down":
-               self.y = self.y + amount
-               self.rect.y = self.y
-               if self.y > self.world_size[1] - Settings.tile_size:
-                    raise OffScreenException
-            elif self.direction == "up":
-                self.y = self.y - amount
-                self.rect.y = self.y
-                if self.y < 0:
-                    raise OffScreenException
+            if self.x + amount.x < 0:
+                raise OffScreenLeftException            
+            elif self.x + amount.x > self.world_size[0] - Settings.tile_size:
+                raise OffScreenRightException
+            elif self.y + amount.y < 0:
+                raise OffScreenTopException
+            elif self.y + amount.y > self.world_size[1] - Settings.tile_size:
+                raise OffScreenBottomException
+            else:
+                self.x += amount.x
+                self.y += amount.y
         except:
             self.scene.despawnObject(self)
             self.beenDeleted = True
-            pass
+            return
 
-        self.collisions = []
         for sprite in self.blocks:
             if sprite is not self:
                 self.collider.rect.x = sprite.x
@@ -112,6 +101,8 @@ class Ranged_Shot(Collidable):
                         Collision(self, sprite)
                     else:
                         self.scene.despawnObject(self)
+        self.rect.x = self.x
+        self.rect.y = self.y
 
 
     def onCollision(self, collision, direction):
@@ -119,3 +110,8 @@ class Ranged_Shot(Collidable):
           self.scene.despawnObject(self)
         except:
             pass
+
+    def imageSelect(self):
+        self.image = pygame.transform.rotate(self.image, Vector3(1,0,0).angle_to(self.direction))
+        if self.direction.y == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
